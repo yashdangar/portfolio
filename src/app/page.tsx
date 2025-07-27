44463dd4ac24ddc94818fa7711b24117e45e80db
yashdangar <yashdangar123@gmail.com>
2025-07-27 16:48:46 +0530

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,44 @@
-import Image from "next/image";
+"use client";
+import {
+  MapPin,
+  BriefcaseBusiness,
+  BookOpen
+} from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+  return (
+    <main className="min-h-screen bg-black text-white p-6 md:p-12 lg:p-24">
+      <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2  2xl:w-1/4 mx-auto">
+          <header className="mb-5 p-2">
+            <h1 className="text-xl md:text-3xl font-bold mb-2">Yash Dangar</h1>
+            <p className="text-gray-400 mb-6 lg:text-md text-xs">
+              Hi , I&apos;m Yash - a software engineer building Elegant
+              interfaces for the modern web
+            </p>
+          </header>
+          <section className="text-xs md:text-sm lg:text-md text-zinc-400 flex flex-col gap-1 mb-5">
+            <p className="flex mb-1 gap-2 items-center italic">
+              <MapPin />
+              Surat, India
+            </p>
+            <p className="flex mb-1 gap-2 items-center italic">
+              <BriefcaseBusiness />
+              Ex.SDE-1 at
+              <a
+                className="underline underline-offset-1 decoration-zinc-600 hover:decoration-current transition"
+                href="https://buffindia.com"
+              >
+                BuffIndia
+              </a>
+            </p>
+            <p className="flex mb-1 gap-2 items-center italic">
+              <BookOpen />
+              B.Tech. from SCET,surat
+            </p>
+          </section>
+      </div>
+    </main>
   );
 }
