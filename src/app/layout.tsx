218069e28020ca4c3bb87ba6082a3f7d7a0a16ef
yashdangar <yashdangar123@gmail.com>
2025-07-28 19:53:26 +0530

    Update favicon and manifest; remove unused vercel.svg file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,7 +81,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
