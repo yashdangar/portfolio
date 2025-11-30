2f32b44b16b3e161af7f03f5c12ddad56dfc3f32
yashdangar <yashdangar123@gmail.com>
2025-11-30 07:40:41 +0530

    Refactor layout width for ProjectPage component

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -117,7 +117,7 @@ export default function ProjectPage() {
 
   return (
     <main className="min-h-screen bg-black text-white p-6 md:p-12 lg:p-24">
-      <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 2xl:w-1/4 mx-auto">
+      <div className="w-full sm:w-5/6 lg:w-1/2 mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
